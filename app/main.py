984c909b364923c63b062ec585226557f209d1d0
abdessattar23 <elyagoubiabdessattar@gmail.com>
2024-11-06 21:45:19 +0100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -15,6 +15,11 @@ def main():
         elif inp.startswith("echo"):
             sys.stdout.write(inp.replace("echo ", "") + "\n")
             sys.stdout.flush()
+        elif inp.startswith("type"):
+            cmd = inp.replace("type ", "");
+            if(cmd == "echo" or cmd == "type" or cmd == "exit"):
+                sys.stdout.write(f"{cmd} is a shell builtin\n")
+                sys.stdout.flush()
         else:
             sys.stdout.write(f"{inp}: command not found\n")
             sys.stdout.flush()
