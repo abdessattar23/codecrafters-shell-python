00e6478b35302ca06e669d05bf39ca7344c49093
abdessattar23 <elyagoubiabdessattar@gmail.com>
2024-11-06 21:36:19 +0100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -3,11 +3,12 @@ import sys
 
 def main():
     # Uncomment this block to pass the first stage
-    sys.stdout.write("$ ")
-    sys.stdout.flush()
+    
 
     # Wait for user input
     while(True):
+        sys.stdout.write("$ ")
+        sys.stdout.flush()
         inp = input("")
         sys.stdout.write(f"{inp}: command not found\n")
         sys.stdout.flush()
