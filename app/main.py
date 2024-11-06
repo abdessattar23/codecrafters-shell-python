c82197ff433e2c678d1c36fee9c1b77a7c96a8f5
abdessattar23 <elyagoubiabdessattar@gmail.com>
2024-11-06 22:04:55 +0100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,10 +1,13 @@
 import sys
+import os
 
 
 def main():
     # Uncomment this block to pass the first stage
     
-
+    path = os.environ.get("PATH")
+    paths = path.split(":")
+    path_cmds = []
     # Wait for user input
     while(True):
         sys.stdout.write("$ ")
@@ -17,11 +20,14 @@ def main():
             sys.stdout.flush()
         elif inp.startswith("type"):
             cmd = inp.replace("type ", "");
+            for pathx in paths:
+                if os.path.isfile(f"{pathx/cmd}"):
+                    path_cmds.append(cmd)
             if(cmd == "echo" or cmd == "type" or cmd == "exit"):
                 sys.stdout.write(f"{cmd} is a shell builtin\n")
                 sys.stdout.flush()
-            else:
-                sys.stdout.write(f"{cmd}: not found\n")
+            elif cmd in path_cmds:
+                sys.stdout.write(f"{cmd} is {path}/{cmd}\n")
                 sys.stdout.flush()
         else:
             sys.stdout.write(f"{inp}: command not found\n")
