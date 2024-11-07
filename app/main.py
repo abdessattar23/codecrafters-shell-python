444711459bd24e4c063b679fbfafb0fefad57f96
root <root@abdeldroid.>
2024-11-07 10:58:36 +0100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -38,7 +38,7 @@ def main():
                     for path in paths:
                         if os.path.isfile(f"{path}/{command.split(' ')[0]}"):
                             os.system(command)
-                            return
+                            continue
                             
                     print(f"{command}: command not found")
 
