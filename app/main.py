4738189262d5ac998ea53f838c210a5bd15dd133
root <root@abdeldroid.>
2024-11-07 11:05:01 +0100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,47 +1,88 @@
-import sys
-import os
+from os.path import basename, expanduser
+from os import getenv, getcwd, chdir
+from subprocess import call
+from typing import Dict
+from glob import glob
 
-def find_in_path(param):
-    path = os.environ['PATH']
-    for directory in path.split(":"):
-        for (dirpath, dirnames, filenames) in os.walk(directory):
-            if param in filenames:
-                return f"{dirpath}/{param}"
-    return None
 
-def main():
+def collect_executables() -> Dict[str, str]:
+    executables = {}
+
+    for directory in getenv('PATH').split(':'):
+        executables.update({
+            basename(executable): directory for executable in glob(directory + '/*') if basename(executable) not in executables
+        })
+
+    return executables
+
+
+def main() -> None:
+    executables = collect_executables()
+    originalwd = getcwd()
+
     while True:
-        sys.stdout.write("$ ")
-        sys.stdout.flush()
-        # Wait for user input
-        command = input()
-        match command.split(" "):
-            case ["exit", "0"]:
-                exit(0)
-            case ["echo", *cmd]:
-                print(" ".join(cmd))
-            case ["type", *cmd]:
-                match cmd:
-                    case ["pwd" | "echo" | "exit" | "type"]:
-                        print(f"{cmd[0]} is a shell builtin")
-                    case _:
-                        location = find_in_path(cmd[0])
-                        if location:
-                            print(f"{cmd[0]} is {location}")
-                        else:
-                            print(f"{' '.join(cmd)}: not found")
-            case _:
-                if command == "pwd":
-                    print(f"{os.getcwd()}")
-                else:
-                    paths = os.environ.get("PATH")
-                    paths = paths.split(":")
-                    for path in paths:
-                        if os.path.isfile(f"{path}/{command.split(' ')[0]}"):
-                            os.system(command)
-                            break
-                    else:
-                        print(f"{command}: command not found")
+        try:
+            line = input('$ ').split(' ')
 
-if __name__ == "__main__":
+            if not line:
+                print('Could not parse input')
+            else:
+                command, arguments = line[0], line[1:]
+
+                if command == 'exit':
+                    exit(
+                        int(arguments[0]) if arguments else 0
+                    )
+                elif command == 'echo':
+                    print(' '.join(arguments))
+                elif command == 'type':
+                    target = arguments[0] if arguments else None
+
+                    if target in ('exit', 'echo', 'type', 'pwd', 'cd'):
+                        print(f'{target} is a shell builtin')
+                    else:
+                        directory = executables.get(target)
+
+                        if directory:
+                            print(f'{target} is {directory}/{target}')
+                        else:
+                            print(f'{target} not found')
+                elif command == 'pwd':
+                    print(getcwd())
+                elif command == 'cd':
+                    directory = arguments[0]
+
+                    try:
+                        chdir(expanduser(directory))
+                    except OSError:
+                        print(f'cd: {directory}: No such file or directory')
+                elif command.startswith('/'):
+                    call(
+                        '{} {}'.format(
+                            command,
+                            ' '.join(arguments)
+                        ),
+                        shell=True
+                    )
+                else:
+                    directory = executables.get(command)
+
+                    if directory:
+                        call(
+                            '{}/{} {}'.format(
+                                directory,
+                                command,
+                                ' '.join(arguments)
+                            ),
+                            shell=True
+                        )
+                    else:
+                        print(f'{command}: command not found')
+        except (KeyboardInterrupt, EOFError):
+            exit(130)
+
+    exit(0)
+
+
+if __name__ == '__main__':
     main()
